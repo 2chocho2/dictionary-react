6e51b2f0d25ae78d99093ad558b35887216e8b13
2chocho2 <demonz734@gmail.com>
2023-05-12 17:59:53 +0900

    0512 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import DictionaryReact from './component/DictionaryReact';
+import { Route, Router, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <DictionaryReact />
+    <>
+      <BrowserRouter>
+        <Route path="/" component={DictionaryReact} />
+      </BrowserRouter>
+    </>
   );
 }
 
